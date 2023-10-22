ac63d332553ac7fe773af14566a7d93f9f3ef3fe
Aleko Georgiev <61247544+unamusedchemical@users.noreply.github.com>
2023-10-22 14:34:43 +0300

    add color to exceptions in main

--- src/main/java/org/chat_client/Main.java
+++ src/main/java/org/chat_client/Main.java
@@ -5,6 +5,10 @@ import org.chat_client.client.ChatClient;
 public class Main {
     public static void main(String[] args) {
         ChatClient client = new ChatClient();
-        client.connect();
+        try {
+            client.connect();
+        } catch (Exception e) {
+            System.err.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
+        }
     }
 }
